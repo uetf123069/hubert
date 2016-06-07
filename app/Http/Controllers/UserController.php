4947ab2ffcb12e84bd0a3401b532526812434a9c
Lazar <lazar@appoets.com>
2016-06-07 15:36:00 +0530

    Stripe Add Card, Delete Card

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,12 @@ use App\Http\Requests;
 
 use App\Helpers\Helper;
 
+use App\Settings;
+
+use App\User;
+
+use App\Cards;
+
 class UserController extends Controller
 {
 
@@ -240,9 +246,64 @@ class UserController extends Controller
             'token' => \Auth::user()->token,
         ]);
 
+        $user_id = \Auth::user()->id;
+        $user = User::find($user_id);
+        $payment_token = $request->stripeToken;
+        $last_four = substr($request->number,-4);
+        $email = \Auth::user()->email;
+        $settings = Settings::where('key' , 'stripe_secret_key')->first();
+                        $stripe_secret_key = $settings->value;
+                \Stripe\Stripe::setApiKey($stripe_secret_key);
+
+        try{
+
+                // Get the key from settings table
+                
+                    $customer = \Stripe\Customer::create(array(
+                                   "card" => $payment_token,
+                                  "email" => $email)
+                                );
+                if($customer){
+
+                    $customer_id = $customer->id;
+
+                    $cards = new Cards;
+                    $cards->user_id = $request->id;
+                    $cards->customer_id = $customer_id;
+                    $cards->last_four = $last_four;
+                    $cards->card_token = $customer->sources->data[0]->id;
+
+                    // Check is any default is available
+                    $check_card = Cards::where('user_id',$request->id)->first();
+
+                    if($check_card ) 
+                        $cards->is_default = 0;
+                    else
+                        $cards->is_default = 1;
+                    
+                    $cards->save();
+
+                    if($user) {
+                        $user->payment_mode = 'card';
+                        $user->default_card = $cards->id;
+                        $user->save();
+                    }
+
+                    $response_array = array('success' => true);
+                    $response_code = 200;
+                
+                } else {
+                    $response->message('Could not create client ID');
+                }
+            
+            } catch(Exception $e) {
+                $response->message('error'. $e);
+            
+            }
+
         $PaymentMethods = $this->UserAPI->get_user_payment_modes($request)->getData();
         
-        return view('user.payment');
+        return back();
     }
 
     /**
