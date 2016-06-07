c3af4b6e2d33958a3fc211261d0881e9f7ea9775
Sidharth <sid@appoets.com>
2016-06-07 16:57:22 +0530

    Layout and cosmetic changes

--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -6,7 +6,7 @@
 <div class="container" id="login-form">
     <a href="{{ url('/') }}" class="login-logo"><img src="{{ asset('logo.png') }}"></a>
     <div class="row">
-        <div class="col-md-4 col-md-offset-4">
+        <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
                 <div class="panel-heading"><h2>Registration Form</h2></div>
                 <div class="panel-body">
