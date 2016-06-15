cc5f66b87bf9ded961d236b0dc85a3cfbe809b5a
ayokesh <ayokesh.cse@gmail.com>
2016-06-15 18:33:43 +0530

    undo changes

--- app/Http/Controllers/ProviderApiController.php
+++ app/Http/Controllers/ProviderApiController.php
@@ -1094,8 +1094,6 @@ class ProviderApiController extends Controller
 	            $requests->provider_status = PROVIDER_SERVICE_COMPLETED;
     			$requests->save();
 
-    			// clearing chat table
-                ChatMessage::where('request_id',$request_id)->delete();
 
     			//Update provider availability
 	            $provider = Provider::find($requests->confirmed_provider);
@@ -1306,9 +1304,6 @@ class ProviderApiController extends Controller
                     /*Update status of the request to cancellation*/
                     $requests->status = REQUEST_CANCELLED;
                     $requests->save();
-
-                    // clearing chat table
-                    ChatMessage::where('request_id',$request_id)->delete();
 
                     // Send Push Notification to User
                     $title = Helper::tr('cancel_by_provider_title');
