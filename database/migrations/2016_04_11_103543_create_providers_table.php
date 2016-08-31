d5ca8e907e8c77e86f05d73ec8b56c42a2219b9c
ayokesh <ayokesh.cse@gmail.com>
2016-08-31 15:31:46 +0530

    migration cleared

--- database/migrations/2016_04_11_103543_create_providers_table.php
+++ database/migrations/2016_04_11_103543_create_providers_table.php
@@ -15,6 +15,8 @@ class CreateProvidersTable extends Migration
         Schema::create('providers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('picture');
